739004fe24a10f17f688f3ad100c0a5fc7b879cc
rajeshn67 <rajeshnarwade67@gmail.com>
2025-03-05 22:53:46 +0530

    Addede

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-
-import './App.css'
-
-import React, { useEffect, useRef, useState } from 'react'
-
+import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
 function App() {
    const [time, settimer]=useState(0);
-
+let timerRef=useRef(null);
 function starttimer(){ 
-   
+  timerRef.current=setInterval(() => {
+     settimer( time=>time + 1);
+ },1000)};
+function stoptimer(){ 
+ {
+    clearInterval(timerRef.current);
+    timerRef.current = null;
+  }
  }
-function stoptimer(){  }
-function resettimer(){  }
+function resettimer(){ 
+stoptimer();
+settimer(0);
+ }
 return (
     <div>
       <h1>Stopwatch : {time} seconds</h1> 
@@ -30,5 +36,4 @@ return (
     </div>
   )
 }
-
-export default App
+export default App;
