f0893051908acf2c5cb1d6ebfabcff144c3face0
rajeshn67 <rajeshnarwade67@gmail.com>
2025-03-05 20:16:28 +0530

    Addede

--- src/App.jsx
+++ src/App.jsx
@@ -4,36 +4,29 @@ import './App.css'
 import React, { useEffect, useRef, useState } from 'react'
 
 function App() {
-   const [count, setcount]=useState(0);
-let val= useRef(0);
-let btnRef=useRef();
-   function handleIncrement(){
-val.current=val.current+1;
-console.log("value of var",val.current);
+   const [time, settimer]=useState(0);
 
-     setcount(count+1);
-   }
-   // it runs on every render
-   useEffect(()=>{
-     console.log('useEffect called');
-   })
-
-   function Changecolor(){
-      btnRef.current.style.backgroundColor='red';
-  }
-    
-  return (
+function starttimer(){ 
+   
+ }
+function stoptimer(){  }
+function resettimer(){  }
+return (
     <div>
-      <button ref={btnRef}  onClick={handleIncrement}>
-         increment
+      <h1>Stopwatch : {time} seconds</h1> 
+      <button onClick={starttimer}>
+         start
       </button>
       <br></br>
-      <button onClick={Changecolor}>
-         Change color
+      <button onClick={stoptimer}>
+        stop
       </button>
-       <div>
-         Count : {count}
-       </div>
+      <br></br>
+      <button onClick={resettimer}>
+         reset
+      </button>
+      
+      
     </div>
   )
 }
